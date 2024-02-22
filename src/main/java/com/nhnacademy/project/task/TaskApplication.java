c606a78e257169be46dece3a28477e93d6e416c9
jjunho50 <jjunho50@gmail.com>
2024-02-22 11:03:40 +0900

    리팩토링 완료

--- src/main/java/com/nhnacademy/project/task/TaskApplication.java
+++ src/main/java/com/nhnacademy/project/task/TaskApplication.java
@@ -3,6 +3,7 @@ package com.nhnacademy.project.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 커밋용 주석
 @SpringBootApplication
 public class TaskApplication {
 	public static void main(String[] args) {
