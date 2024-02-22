35fe7e366432f8b44a58956b3f11e78bad357058
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-02-22 19:50:33 +0900

    objectMapper 및 일부 수정

--- src/test/java/com/nhnacademy/project/task/controller/TaskControllerTest.java
+++ src/test/java/com/nhnacademy/project/task/controller/TaskControllerTest.java
@@ -33,12 +33,15 @@ class TaskControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     private TaskRepository repository;
 
     @Test
     void getAllTasks() throws Exception {
-        given(repository.findAllBy()).willReturn(List.of(new TaskDto(1, "taskTitle", "jjunho50")));
+        given(repository.getAllBy()).willReturn(List.of(new TaskDto(1, "taskTitle", "jjunho50")));
 
         mockMvc.perform(get("/task"))
                 .andExpect(status().isOk())
@@ -48,7 +51,7 @@ class TaskControllerTest {
 
     @Test
     void findByTaskId() throws Exception {
-        given(repository.findByTaskId(1)).willReturn(Optional.of(new TaskDto(1, "taskTitle", "jjunho50")));
+        given(repository.getByTaskId(1)).willReturn(Optional.of(new TaskDto(1, "taskTitle", "jjunho50")));
 
         mockMvc.perform(get("/task/{id}", 1L))
                 .andExpect(status().isOk())
@@ -59,11 +62,8 @@ class TaskControllerTest {
 
     @Test
     void createTask() throws Exception {
-        Project project = new Project("jjunho50", "name", "state");
+        Project project = new Project(1, "jjunho50", "name", "state");
         Task task = new Task(1, project, "jjunho50", "title", "content", LocalDateTime.now());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
 
         given(repository.save(any(Task.class)))
                 .willReturn(new Task(1, project, "jjunho50", "taskTitle", "jjunho50", LocalDateTime.now()));
@@ -77,11 +77,9 @@ class TaskControllerTest {
 
     @Test
     void updateComment() throws Exception {
-        Project project = new Project("jjunho50", "name", "state");
+        Project project = new Project(1,"jjunho50", "name", "state");
         Task task = new Task(1, project, "jjunho50", "title", "content", LocalDateTime.now());
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
         String updatedProjectJson = objectMapper.writeValueAsString(task);
 
         mockMvc.perform(
