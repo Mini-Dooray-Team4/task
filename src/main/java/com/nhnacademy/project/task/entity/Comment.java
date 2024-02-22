23eb5f5e3c8538a5f883db635eb9907241e26830
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-02-22 16:20:24 +0900

    projectDto, CommentDto 작성중

--- src/main/java/com/nhnacademy/project/task/entity/Comment.java
+++ src/main/java/com/nhnacademy/project/task/entity/Comment.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "comments") //test
+@Table(name = "comments")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
