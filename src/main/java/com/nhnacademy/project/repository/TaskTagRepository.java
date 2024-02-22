d95d5c49f44b8a1cfa60b917835973157a742822
jjunho50 <jjunho50@gmail.com>
2024-02-22 09:21:29 +0900

    간단한 entity, repository, service 완료

--- src/main/java/com/nhnacademy/project/repository/TaskTagRepository.java
+++ src/main/java/com/nhnacademy/project/repository/TaskTagRepository.java
@@ -3,5 +3,5 @@ package com.nhnacademy.project.repository;
 import com.nhnacademy.project.entity.TaskTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskTagRepository extends JpaRepository<TaskTag, TaskTag.pk> {
+public interface TaskTagRepository extends JpaRepository<TaskTag, TaskTag.Pk> {
 }
