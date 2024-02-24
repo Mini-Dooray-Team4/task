ba8f28ded3548032093f3e42ca6dbfcfedc6e2d4
jjunho50 <jjunho50@gmail.com>
2024-02-24 17:34:22 +0900

    Task(milestone) 관련 테스트 수정

---
+++ src/test/java/com/nhnacademy/project/task/service/impl/CommentServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.project.task.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServiceImplTest {
+
+    @Test
+    void getAllComments() {
+    }
+
+    @Test
+    void getComment() {
+    }
+
+    @Test
+    void createComment() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
+
+    @Test
+    void updateComment() {
+    }
+}
